2be3d6ef808b90472fc61ebcc3ddccf353531897
dalogax <dalogax@gmail.com>
2018-05-23 00:34:28 +0200

    first commit

---
+++ src/main/java/com/dalogax/sample/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.dalogax.sample.controller;
+
+import java.util.List;
+
+import com.dalogax.sample.model.User;
+import com.dalogax.sample.repository.UserRepository;
+import com.google.common.collect.Lists;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public List<User> getAll() {
+        return Lists.newArrayList(userRepository.findAll());
+    }
+
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
+    public User getById(@PathVariable("id") int id) {
+        return userRepository.findUserById(id);
+    }
+
+    //RestTemplate restTemplate = new RestTemplate();
+    //URI uri = URI.create("http://localhost:8090/recommended");
+    //restTemplate.getForObject(uri, String.class);
+}
---
+++ src/test/java/com/dalogax/sample/SampleApplicationTests.java
@@ -0,0 +1,52 @@
+package com.dalogax.sample;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import com.dalogax.sample.model.User;
+import com.dalogax.sample.repository.UserRepository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleApplicationTests {
+
+	@Autowired
+    private UserRepository userRepository;
+
+	@Test
+	public void contextLoads() {
+	}
+ 
+    @Test
+    public void findAllUsers() {
+        List<User> users = userRepository.findAll();
+        assertNotNull(users);
+        assertTrue(!users.isEmpty());
+    }
+ 
+    @Test
+    public void findUserById() {
+        User user = userRepository.findUserById(1);
+        assertNotNull(user);
+    }
+ 
+    @Test
+    public void createUser() {
+        User user = new User(0, "John", "john@gmail.com");
+        User savedUser = userRepository.create(user);
+        User newUser = userRepository.findUserById(savedUser.getId());
+        assertNotNull(newUser);
+        assertEquals("John", newUser.getName());
+        assertEquals("john@gmail.com", newUser.getEmail());
+    }
+
+}

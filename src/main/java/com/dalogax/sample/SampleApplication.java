cad52380cb26d2c0f481cd55d6c05ea044bafae8
dalogax <dalogax@gmail.com>
2018-05-25 00:30:19 +0200

    fixed config

--- src/main/java/com/dalogax/sample/SampleApplication.java
+++ src/main/java/com/dalogax/sample/SampleApplication.java
@@ -1,4 +1,4 @@
-package com.dalogax.sample.config;
+package com.dalogax.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
